2c410e1362307336af1e9e1970dce3725c5e3d61
lopera.william@gmail.com <William>
2022-05-30 10:18:57 -0500

    modificar y borrar user

--- src/service.js
+++ src/service.js
@@ -1,7 +1,13 @@
-const data = require("./MOCK_DATA.json");
+let data = require("./MOCK_DATA.json");
 
 module.exports = {
-  getUser: () => data,
+  getUsers: () => data,
+  getUser: (id) => {
+    const identifier = Number(id); // parseint(id)
+    // const user = data.filter((user) => user.id === identifier)[0];
+    const user = data.find((user) => user.id === identifier);
+    return user;
+  },
   createUser: (dataUser) => {
     // Data solo en memoria
 
@@ -11,5 +17,19 @@ module.exports = {
     };
     data.push(newUser);
     return newUser;
+  },
+  updateUser: (id, dataUser) => {
+    const identifier = Number(id);
+    const index = data.findIndex((user) => user.id === identifier);
+    data[index] = {
+      id: id,
+      ...dataUser,
+    };
+    return data[index];
+  },
+  deleteUser: (id) => {
+    const identifier = Number(id);
+    data = data.filter((user) => user.id !== identifier);
+    return id;
   },
 };
